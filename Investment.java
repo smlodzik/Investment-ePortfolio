e6022ea39ae58753c112203d4a4f73adafb3cba4
Sophie Mlodzik <45125403+smlodzik@users.noreply.github.com>
2021-11-07 22:54:34 -0500

    create investment.java
    investment super class

---
+++ Investment.java
@@ -0,0 +1,68 @@
+package ePortfolio2;
+
+
+public class Investment {
+    public String symbol;
+    public String name;
+    public int quantity;
+    public double price;
+    public double bookValue;
+
+    public Investment(String inputSymbol, String inputName, int inputQuantity, double inputPrice, double inputBookValue) {
+        this.symbol = inputSymbol;
+        this.name = inputName;
+        this.quantity = inputQuantity;
+        this.price = inputPrice;
+        this.bookValue = inputBookValue;
+    }
+
+    public void setSymbol(String inputSymbol){
+        this.symbol = inputSymbol;
+    }
+
+    public String getSymbol(){
+        return symbol;
+    }
+
+
+    public void setName(String inputName){
+        this.name = inputName;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+
+    public void setQuantity(int inputQuantity){
+        this.quantity = inputQuantity;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+
+    public void setPrice(double inputPrice){
+        this.price = inputPrice;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+
+    public void setBookValue(double inputBookValue){
+        this.bookValue = inputBookValue;
+    }
+
+    public double getBookValue(){
+        return bookValue;
+    }
+
+
+    @Override
+    public String toString(){
+        return "Symbol: " + this.symbol + "\nName: " + this.name + "\nQuantity: " + this.quantity + "\nPrice: $" + this.price + "\nBook Value: $" + this.bookValue;
+    }
+}
